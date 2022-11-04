a3f2d1322fd56a49224cdd5be6ea9d1e90866be9
Somansh Gupta <somanshgupta12@gmail.com>
2022-11-04 15:29:09 +0530

    use image in jpg format which are present in our code and this method is in catche fomat means it is dont loaded in chrome again when we open morning and then in evening

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import pageNotFound from "./pageNotFound.jpg";
 
 export function NotFound() {
   return (
     <div>
-      <img src="/images/pageNotFound.jpg" />
+      <img src={pageNotFound} />
       <Link to={"/"} className="px-2 text-5xl text-center bg-red-300 ">
         <div className="px-2 mx-3 my-4 text-3xl text-indigo-500 bg-red-300">
           back
