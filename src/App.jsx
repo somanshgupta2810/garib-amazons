6432f55879cebbfd6182a7c45f7dea044e264383
somansh gupta <somanshgupta12@gmail.com>
2022-11-24 07:13:43 +0530

    login page made by form tag and by useStates

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,59 @@
-import React,{useState} from 'react';
-import Maindetail from './Maindetail';
+import React, { useState } from "react";
+import Maindetail from "./Maindetail";
 import { Routes, Route, Link } from "react-router-dom";
-import Productlist from './Productlist';
-import ProductDetail from './ProductDetail';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import {NotFound} from './NotFound';
+import Productlist from "./Productlist";
+import ProductDetail from "./ProductDetail";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { NotFound } from "./NotFound";
+import Login from "./Login";
+
 function App() {
-  const savedDataString=localStorage.getItem("my cart")||"{}";/*esee hmm nikal rahe ha*/                                                                              
- const cartObject=JSON.parse(savedDataString);/*esee hmm string ko vaps sa objct form ma kar rahe ha*/
+  const savedDataString =
+    localStorage.getItem("my cart") || "{}"; /*esee hmm nikal rahe ha*/
+  const cartObject =
+    JSON.parse(
+      savedDataString
+    ); /*esee hmm string ko vaps sa objct form ma kar rahe ha*/
 
-  
-const  [Cart,SetCart]=useState(cartObject);
-  console.log("Crt is",Cart);
-  
-  function ProductAmount(productId,count){
-    const oldCount=Cart[productId]||0;/*agar jo chij hmee dalne ha vo present ha he nahi to zero ho ayga*/
-    const newCart={...Cart,[productId]:oldCount+count};
-    
-  SetCart(newCart);
-    const cartString=JSON.stringify(newCart);/*ya hmm ess liya kar rahe ha kyu ki local storage ma hmm string format ma he likh skte ha*/
-    localStorage.setItem("my cart",cartString)/*esse hmm save kar rahe ha*/
-  ;}
-    
-   const totalCount=Object.keys(Cart).reduce(function(previous,current) {return previous + Cart[current];},0)
-    
-  
-return(
-  <div className="flex h-screen flex-col">
-    <Navbar productAcount={totalCount}/>
-    <div className="grow">
-      
-    <Routes >
-      
- <Route index element={<Maindetail/>}></Route>
- <Route path="/ProductDetails/:id" element={<ProductDetail HandleOnclick={ProductAmount}/>} ></Route>
- <Route path="*" element={<NotFound/>}></Route>
-      </Routes>
-      </div>
-  <Footer/>
-  </div>
+  const [Cart, SetCart] = useState(cartObject);
+  console.log("Crt is", Cart);
 
-)
+  function ProductAmount(productId, count) {
+    const oldCount =
+      Cart[productId] ||
+      0; /*agar jo chij hmee dalne ha vo present ha he nahi to zero ho ayga*/
+    const newCart = { ...Cart, [productId]: oldCount + count };
+
+    SetCart(newCart);
+    const cartString =
+      JSON.stringify(
+        newCart
+      ); /*ya hmm ess liya kar rahe ha kyu ki local storage ma hmm string format ma he likh skte ha*/
+    localStorage.setItem("my cart", cartString) /*esse hmm save kar rahe ha*/;
   }
 
+  const totalCount = Object.keys(Cart).reduce(function (previous, current) {
+    return previous + Cart[current];
+  }, 0);
+
+  return (
+    <div className="flex h-screen flex-col">
+      <Navbar productAcount={totalCount} />
+      <div className="grow">
+        <Routes>
+          <Route index element={<Maindetail />}></Route>
+          <Route
+            path="/ProductDetails/:id"
+            element={<ProductDetail HandleOnclick={ProductAmount} />}
+          ></Route>
+          <Route path="*" element={<NotFound />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,48 @@
 import React from "react";
+import { useState } from "react";
 
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState();
 
-function Login(){
-console.log(this is my);
+  function callLoginApi(event) {
+    event.preventDefault(),
+      console.log("email is", email, "password is", password);
+  }
 
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
 
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
 
-
+  return (
+    <form
+      onSubmit={callLoginApi}
+      className="flex h-screen w-full justify-center place-items-center bg-gray-300"
+    >
+      <div className="flex flex-col bg-white p-10 rounded-lg">
+        <h1 className="text-3xl font-extrabold my-5">Login To Garib-Amazon</h1>
+        <input
+          className="h-10 border-2"
+          placeholder="Enter The Email Address"
+          onChange={handleEmailChange}
+          type="email"
+        />
+        <input
+          className="h-10 border-2"
+          placeholder="Enter The Password"
+          onChange={handlePasswordChange}
+          type="password"
+        />
+        <button className="text-2xl bg-purple-600 text-white mt-8">
+          Login
+        </button>
+      </div>
+    </form>
+  );
 }
+
+export default Login;
